95b9366c60d2790d545a7b529dedf27d81961304
angeltsvetkov <mrproper86@gmail.com>
2017-01-19 12:09:03 +0200

    Update ignored files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     botService = require('server/services/bot'),
     settingsService = require('server/services/settings'),
-    installService = require('server/services/install'),
     hookService = require('server/services/hook'),
     bodyParser = require('body-parser'),
     config = require('config');
