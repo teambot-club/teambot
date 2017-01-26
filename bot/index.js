6779812fa7461e4ec8697bd66e9e6e60adcc1a10
Dimitar Tachev <Dimitar.Tachev@telerik.com>
2017-01-26 12:02:43 +0200

    Support web hooks and interactive messages per skill. #22

--- bot/index.js
+++ bot/index.js
@@ -1,7 +1,7 @@
 var _bots = null,
     hasOauthServer = false;
 
-var teambot = function() {
+var teambot = function () {
     var Botkit = require('botkit'),
         skillsLoader = require('bot/skills-loader'),
         settingsProvider = require('server/providers/settings-provider'),
@@ -12,7 +12,7 @@ var teambot = function() {
 
     function start(callback, devSkill) {
 
-        settingsProvider.getByScope('general', function(err, config) {
+        settingsProvider.getByScope('general', function (err, config) {
             var mongoUri = config.mongoUri,
                 mongoStorage = require('botkit-storage-mongo')({ mongoUri: mongoUri });
 
@@ -21,8 +21,8 @@ var teambot = function() {
                     levels: winston.config.syslog.levels,
                     transports: [
                         process.env.NODE_ENV == 'production' ?
-                        new(winston.transports.MongoDB)({ "db": mongoUri + "/admin" }) :
-                        new(winston.transports.Console)()
+                            new (winston.transports.MongoDB)({ "db": mongoUri + "/admin" }) :
+                            new (winston.transports.Console)()
                     ]
                 }),
                 interactive_replies: true,
@@ -35,9 +35,9 @@ var teambot = function() {
 
             var middleware = require('bot/middleware');
 
-            skillsLoader.installPredefinedSkills(controller, middleware, devSkill, function() {
+            skillsLoader.installPredefinedSkills(controller, middleware, devSkill, function () {
 
-                settingsProvider.getByScope('slack', function(err, config) {
+                settingsProvider.getByScope('slack', function (err, config) {
 
                     if (!config || !config.clientId || !config.clientSecret || !config.redirectUri) {
                         console.log("Slack app is not configured!");
@@ -92,7 +92,7 @@ var teambot = function() {
     }
 
     function loadLocalUsers() {
-        controller.storage.users.all(function(err, users) {
+        controller.storage.users.all(function (err, users) {
             if (err) {
                 return;
             }
@@ -112,13 +112,13 @@ var teambot = function() {
                 scopes: ['bot']
             });
 
-            controller.setupWebserver(3000, function() {
+            controller.setupWebserver(3000, function () {
                 controller.createWebhookEndpoints(controller.webserver);
-                controller.createOauthEndpoints(controller.webserver, function(err, req, res) {
+                controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
                     if (err) {
                         res.status(500).send(err);
                     } else {
-                        settingsProvider.getByScope('general', function(err, config) {
+                        settingsProvider.getByScope('general', function (err, config) {
                             loadLocalUsers();
                             res.redirect(req.protocol + '://' + req.hostname + ':' + config.server.port + '/#/done');
                         });
@@ -131,19 +131,42 @@ var teambot = function() {
 
         function trackBot(bot) {
             _bots[bot.config.token] = bot;
+            controller.on('interactive_message_callback', function postInteractiveMessage(bot, message) {
+                var callbackParts = message.callback_id.split(":");
+                var skill = callbackParts[0];
+                var buttonsGroup = callbackParts[1];
+
+                try {
+                    require(skill);
+                } catch (ex) {
+                    return bot.reply(message, "Invalid callbackId '" + message.callback_id + "'. The '" + skill + "' skill is not installed.");
+                }
+
+                try {
+                    var targetButtonsFunction = require(skill + "/buttons");
+                } catch (ex) {
+                    return bot.reply(message, "Invalid callbackId '" + message.callback_id + "'. The '" + skill + "' skill does not support button clicks handling.");
+                }
+
+                try {
+                    targetButtonsFunction(buttonsGroup, message, controller, bot);
+                } catch (ex) {
+                    return bot.reply(message, "Invalid callbackId '" + message.callback_id + "'. An exception occurred during button click handling: '" + ex.message + "'");
+                }
+            });
         }
 
-        controller.on('create_bot', function(bot, config) {
+        controller.on('create_bot', function (bot, config) {
 
             if (_bots && _bots[bot.config.token]) {
                 // already online! do nothing.
             } else {
-                bot.startRTM(function(err) {
+                bot.startRTM(function (err) {
                     if (!err) {
                         trackBot(bot);
                     }
 
-                    bot.startPrivateConversation({ user: config.createdBy }, function(err, convo) {
+                    bot.startPrivateConversation({ user: config.createdBy }, function (err, convo) {
                         if (err) {
                             log(err);
                         } else {
@@ -155,7 +178,7 @@ var teambot = function() {
             }
         });
 
-        controller.storage.teams.all(function(err, teams) {
+        controller.storage.teams.all(function (err, teams) {
             if (err) {
                 throw new Error(err);
             }
@@ -163,7 +186,7 @@ var teambot = function() {
             // connect all teams with bots up to slack!
             for (var t in teams) {
                 if (teams[t].bot) {
-                    controller.spawn(teams[t]).startRTM(function(err, bot) {
+                    controller.spawn(teams[t]).startRTM(function (err, bot) {
                         if (err) {
                             log('Error connecting bot to Slack:', err);
                         } else {
@@ -196,6 +219,6 @@ var teambot = function() {
         log: log
     };
 
-}();
+} ();
 
 module.exports = teambot;
